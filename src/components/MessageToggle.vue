69a6d18606ab95b4fda82548ce76e7cf7a4bfddf
ly2011 <1518550424@qq.com>
2018-07-06 16:40:22 +0800

    实现测试点击事件

---
+++ src/components/MessageToggle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <message :msg="msg" />
+    <button id="toggle-message" @click="toggleMessage">
+      Change message
+    </button>
+  </div>
+</template>
+
+<script>
+import Message from '@/components/Message'
+export default {
+  name: 'message-toggle',
+  components: {
+    Message
+  },
+  data () {
+    return {
+      msg: null
+    }
+  },
+  methods: {
+    toggleMessage() {
+      this.msg = this.msg === 'message' ? 'toggled message' : 'message'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
