1cffaf3f9dece626e021ca4fb3922ef7bffa118f
ly2011 <1518550424@qq.com>
2018-07-06 16:15:27 +0800

    添加Message和List组件以及测试用例并调整coverage的生成目录

---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li :key="item" v-for="item in items">
+      {{item}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  props: [
+    'items'
+  ]
+}
+</script>
+
+<style>
+</style>
